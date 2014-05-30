ed8291b22ba4d60f7b174feac618e9547161d728
sobek44 <sobbombo@gmail.com>
2014-05-30 17:21:02 +0200

    extracting code chunks in MPCC

--- R/MPCV_res.R
+++ R/MPCV_res.R
@@ -12,14 +12,13 @@
 #' @param scale Should data be scaled?
 #' @return a list consisting of
 #' \item{segmentation}{of points to clusters}
-#' \item{BIC}{Value of BIC criterion}
+#' \item{BIC}{Value of \code{\link{myBIC}} criterion}
 MPCV.reps <- function(X, numb.Clusters=2, numb.runs=20, stop.criterion=1, max.iter=20, max.dim=1, method='h', scale=T){
   if(scale){
     dane = scale(X)
   }
   else dane= X
-  #goodness of clustering 
-  missclassifications <- NULL
+  
   #goodness of fit
   Hs <- NULL
   Res <- NULL
@@ -51,10 +50,9 @@ MPCV.reps <- function(X, numb.Clusters=2, numb.runs=20, stop.criterion=1, max.it
       } #otherwise we have a perfect fit
     }
     
-    Hs[i] = H
-    Res[i] = R 
-    #missclassifications[i] <- missclassify.heuristic(current.segmentation, dim(dane)[2]/numb.Clusters, n=numb.Clusters)
-    BICs[i] <- myBIC(dane, current.segmentation,max.dim, numb.Clusters)
+    Hs[i] = H #sum.explained.variance(dane, current.segmentation, max.dim, numb.Clusters)
+    Res[i] = R #sum.residuals(dane, current.segmentation, max.dim, numb.Clusters)
+    BICs[i] <- myBIC(dane, current.segmentation, max.dim, numb.Clusters)
     segmentations[[i]] <- current.segmentation
     if(i%%10==0) print(paste("Done ", i))
   }  
@@ -63,9 +61,38 @@ MPCV.reps <- function(X, numb.Clusters=2, numb.runs=20, stop.criterion=1, max.it
                 #misclassification = missclassifications[which.max(Hs)],
                 BIC = BICs[which.max(Hs)]))
   }
-  else{
+  else if(method=='r'){
     return(list(segmentation = segmentations[[which.min(Res)]],
                 #misclassification = missclassifications[which.min(Res)],
                 BIC = BICs[which.min(Res)]))
   }
+  else{
+    return(list(segmentation = segmentations[[which.max(BICs)]],
+                BIC = BICs[which.max(BICs)]))
+  }
+}
+
+sum.explained.variance <- function(dane, current.segmentation, max.dim, numb.Clusters){
+  H <- 0
+  for (j in 1:numb.Clusters){
+    if(is.matrix(dane[,current.segmentation==j]) && dim(dane[,current.segmentation==j])[2]>max.dim){
+      a <- summary(prcomp(dane[,current.segmentation==j]))
+      H <- H + a$importance[3, max.dim]
+    }
+    else{
+      H <- H+1 #all variance in that cluster is explained
+    }
+  }
+  return(H)
+}
+
+sum.residuals <- function(dane, current.segmentation, max.dim, numb.Clusters){
+  R <- 0
+  for (j in 1:numb.Clusters){
+    if(is.matrix(dane[,current.segmentation==j]) && dim(dane[,current.segmentation==j])[2]>max.dim){
+      a <- summary(prcomp(dane[,current.segmentation==j]))
+      R <- R + sum(lm(dane[,current.segmentation==j] ~ current.pcas[[j]])$residuals^2)
+    } #otherwise we have a perfect fit
+  }
+  return(R)
 }
