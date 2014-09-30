da3cdcbeef76b57bb37a4edf7d98dac51ba5b34d
sobek44 <sobbombo@gmail.com>
2014-09-30 15:23:40 +0200

    speeding up using doMC package

--- R/MPCV_res.R
+++ R/MPCV_res.R
@@ -19,12 +19,11 @@ MPCV.reps <- function(X, numb.Clusters=2, numb.runs=20, stop.criterion=1, max.it
     # create progress bar
     pb <- txtProgressBar(min = 0, max = numb.runs, style = 3)
   }
-  
   method <- match.arg(method)
   if(scale){
     dane = scale(X)
   }
-  else dane= X
+  else dane=X
   
   #goodness of fit
   Hs <- NULL
@@ -32,33 +31,36 @@ MPCV.reps <- function(X, numb.Clusters=2, numb.runs=20, stop.criterion=1, max.it
   BICs <- NULL 
   
   segmentations <- NULL
-  for(i in 1:numb.runs){
-    H <- 0 #explained variance
-    R <- 0 #residual variance
-    
-    MPCV.res <- MPCV(X=dane, numberClusters=numb.Clusters, maxSubspaceDim=max.dim)
+  foreach(i=1:numb.runs) %dopar% {
+    MPCV.res <- MPCV(X=dane, numberClusters=numb.Clusters, maxSubspaceDim=max.dim, max.iter=max.iter)
     current.segmentation <- MPCV.res$segmentation
     current.pcas <- MPCV.res$pcas
-   
-    for (j in 1:numb.Clusters){
-      if(is.matrix(dane[,current.segmentation==j]) && dim(dane[,current.segmentation==j])[2]>max.dim){
-        a <- summary(prcomp(dane[,current.segmentation==j]))
-        H <- H + a$importance[3,max.dim]
+    
+    if(method=="singular"){
+      H <- 0 #explained variance
+      for (j in 1:numb.Clusters){
+        if(is.matrix(dane[,current.segmentation==j]) && dim(dane[,current.segmentation==j])[2]>max.dim){
+          a <- summary(prcomp(dane[,current.segmentation==j]))
+          H <- H + a$importance[3,max.dim]
+        }
+        else{
+          H <- H+1 #all variance in that cluster is explained
+        }
       }
-      else{
-        H <- H+1 #all variance in that cluster is explained
-      }
+      Hs[i] = H #sum.explained.variance(dane, current.segmentation, max.dim, numb.Clusters)
     }
     
-    for (j in 1:numb.Clusters){
-      if(is.matrix(dane[,current.segmentation==j]) && dim(dane[,current.segmentation==j])[2]>max.dim){
-        a <- summary(prcomp(dane[,current.segmentation==j]))
-        R <- R + sum(lm(dane[,current.segmentation==j] ~ current.pcas[[j]])$residuals^2)
-      } #otherwise we have a perfect fit
+    if(method=="residual"){
+      R <- 0 #residual variance
+      for (j in 1:numb.Clusters){
+        if(is.matrix(dane[,current.segmentation==j]) && dim(dane[,current.segmentation==j])[2]>max.dim){
+          a <- summary(prcomp(dane[,current.segmentation==j]))
+          R <- R + sum(lm(dane[,current.segmentation==j] ~ current.pcas[[j]])$residuals^2)
+        } #otherwise we have a perfect fit
+      }
+      Res[i] = R #sum.residuals(dane, current.segmentation, max.dim, numb.Clusters)
     }
     
-    Hs[i] = H #sum.explained.variance(dane, current.segmentation, max.dim, numb.Clusters)
-    Res[i] = R #sum.residuals(dane, current.segmentation, max.dim, numb.Clusters)
     BICs[i] <- myBIC(dane, current.segmentation, max.dim, numb.Clusters)
     segmentations[[i]] <- current.segmentation
     if(verbose) setTxtProgressBar(pb, i)
