2b5225b7716b8a0bdd513021cf032613538dddcd
sobek44 <sobbombo@gmail.com>
2014-11-01 09:19:19 +0100

    more new documentation

--- R/createDATA.R
+++ R/createDATA.R
@@ -1,9 +1,11 @@
 #' Simulating subspace clustering data
 #' 
-#' Generating random data for subspace clustering simulation
+#'  Generating data for simulation with a low-rank subspace structure: 
+#'  variables are clustered and each cluster has a low-rank representation
 #' 
 #' @param n an integer, number of individuals
-#' @param SNR a double, signal to noise ratio measured as variance of variable to variance of noise
+#' @param SNR a numeric, signal to noise ratio measured as variance of the variable, element of a subspace,
+#'        to the variance of noise
 #' @param K an integer, number of subspaces
 #' @param numb.vars an integer, number of variables in each subspace
 #' @param max.dim an integer, if equal.dims is TRUE then max.dim is dimension of each subspace.
@@ -17,26 +19,26 @@
 #' \item{s}{vector, true partiton of variables}
 #' @examples
 #' data <- data.simulation()
-#' data2 <- data.simulation(n=30, SNR=2, K=5, numb.vars=20, max.dim=3, equal.dims=FALSE)
-data.simulation <- function(n = 100, SNR=1, K = 10, numb.vars = 30, max.dim = 2, equal.dims=T){
-  #draw dimensions of subspaces
-  sigma = 1/SNR
+#' data2 <- data.simulation(n = 30, SNR = 2, K = 5, numb.vars = 20, max.dim = 3, equal.dims = FALSE)
+data.simulation <- function(n = 100, SNR = 1, K = 10, numb.vars = 30, max.dim = 2, equal.dims = TRUE){
+  sigma <- 1/SNR
+  #subspaces dimensions depend on equal.dims value
   if(equal.dims)
-    dims = rep(max.dim,K)
+    dims <- rep(max.dim,K)
   else
-    dims = sample(1:max.dim, K, replace=T)   
+    dims <- sample(1:max.dim, K, replace=T)   
   
-  X = NULL
-  Y = NULL
-  s = NULL
+  X <- NULL
+  Y <- NULL
+  s <- NULL
   for (j in 1:K){
     Z <- qr.Q(qr(replicate(dims[j], rnorm(n, 0, 1))))
     coeff <- matrix(runif(dims[j]*numb.vars, 0.1, 1) * sign(runif(dims[j]*numb.vars, -1, 1)), nrow=dims[j])
     SIGNAL <- Z %*% coeff
     SIGNAL <- scale(SIGNAL)
-    Y = cbind(Y,SIGNAL)
-    X = cbind(X, SIGNAL + replicate(numb.vars, rnorm(n, 0, sigma)))
-    s = c(s, rep(j, numb.vars))
+    Y <- cbind(Y,SIGNAL)
+    X <- cbind(X, SIGNAL + replicate(numb.vars, rnorm(n, 0, sigma)))
+    s <- c(s, rep(j, numb.vars))
   }
   return(list(X = X,
               signals = Y,
