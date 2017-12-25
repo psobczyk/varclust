90ee85a6b0e917a38cc9daf9e9e6096a5a9766ec
sjwilczynski <sjwilczynski@gmail.com>
2017-12-25 23:14:14 +0100

    update tests

--- tests/testthat/tests_auxiliary_functions.R
+++ tests/testthat/tests_auxiliary_functions.R
@@ -18,13 +18,12 @@ test_that("integration",{
   expect_equal(varclust::integration(part2,part1)[2], 0.5 )
 })
 
-
-test_that("choose cluster", {
+test_that("choose cluster BIC", {
   sim.data <- varclust::data.simulation(n = 100, SNR = 1, K = 2, numb.vars = 4, max.dim = 2)
   pca1 <- summary(prcomp(x=sim.data$signals[,1:4]))$x[,1:2]
   pca2 <- summary(prcomp(x=sim.data$signals[,5:8]))$x[,1:2]
-  expect_equal(varclust:::choose.cluster(sim.data$signals[,1], list(pca1, pca2), 2), 1)
-  expect_equal(varclust:::choose.cluster(sim.data$signals[,5], list(pca1, pca2), 2), 2)
+  expect_equal(varclust:::choose.cluster.BIC(sim.data$signals[,1], list(pca1, pca2), 2), 1)
+  expect_equal(varclust:::choose.cluster.BIC(sim.data$signals[,5], list(pca1, pca2), 2), 2)
 })
 
 test_that("choose cluster BIC - when n is large we can choose the right model", {
@@ -32,42 +31,15 @@ test_that("choose cluster BIC - when n is large we can choose the right model", 
   pca1 <- summary(prcomp(x=sim.data$X[,1:10]))$x[,1:2]
   pca2 <- summary(prcomp(x=sim.data$X[,11:20]))$x[,1:2]
   expect_equal(varclust:::choose.cluster.BIC(sim.data$X[,1], list(pca1, pca2), 2), 1)
-  expect_equal(varclust:::choose.cluster(sim.data$X[,11], list(pca1, pca2), 2), 2)
+  expect_equal(varclust:::choose.cluster.BIC(sim.data$X[,11], list(pca1, pca2), 2), 2)
 })
 
-# test_that("choose cluster BIC - warning for suspiciously low noise", {
-#   sim.data <- varclust::data.simulation(n = 100, SNR = 1, K = 2, numb.vars = 4, max.dim = 2)
-#   pca1 <- summary(prcomp(x=sim.data$signals[,1:4]))$x[,1:2]
-#   pca2 <- summary(prcomp(x=sim.data$signals[,1:4]))$x[,1:3]
-#   expect_warning(varclust:::choose.cluster.BIC(sim.data$signals[,1], list(pca1, pca2), 2))
-# })
-
-
-test_that("adjusted cluster BIC - is computed correctly. Works with mlcc.reps", {
-  set.seed(1)
-  sim.data <- varclust::data.simulation(n = 50, SNR = 1, K = 2, numb.vars = 20, max.dim = 2)
-  expect_equal(varclust:::adjusted.cluster.BIC(scale(sim.data$X), sim.data$s, c(2,2), 2), 
-               -2457.157, tolerance = 1e-3, scale = 1)
-  #please note that for the mlcc.fit value is -2445.326
-  #the difference is because of sigma estimation. In mlcc.fit we get smaller sigma because
-  #we estimate it with max.dim, hence BIC is higher.
-})
-
-test_that("get sigma", {
-  set.seed(1)
-  sim.data <- varclust::data.simulation(n = 50, SNR = 1, K = 2, numb.vars = 20, max.dim = 2)
-  sigma2 <- varclust:::getSigma(X = scale(sim.data$X), segmentation = sim.data$s, max.dim = 2, 
-                     n = 50, p = 2*20, numb.clusters = 2)
-  sigma3 <- varclust:::getSigma(X = scale(sim.data$X), segmentation = sim.data$s, max.dim = 3, 
-                     n = 50, p = 2*20, numb.clusters = 2)
-  expect_gt(sigma2, sigma3)
-  sigma2_sig <- varclust:::getSigma(X = scale(sim.data$signals), segmentation = sim.data$s, max.dim = 2, 
-                               n = 50, p = 2*20, numb.clusters = 2)
-  expect_equal(sigma2_sig, 0)
-  # the values below were computed using version 0.9.19
-  expect_equal(sigma2, 0.7088557, tolerance = 1e-7, scale = 1)
-  expect_equal(sigma3, 0.6795928, tolerance = 1e-7, scale = 1)
-})
+ test_that("choose cluster BIC - warning for suspiciously low noise", {
+   sim.data <- varclust::data.simulation(n = 100, SNR = 1, K = 2, numb.vars = 4, max.dim = 2)
+   pca1 <- summary(prcomp(x=sim.data$signals[,1:4]))$x[,1:2]
+   pca2 <- summary(prcomp(x=sim.data$signals[,1:4]))$x[,1:3]
+   expect_warning(varclust:::choose.cluster.BIC(sim.data$signals[,1], list(pca1, pca2), 2, TRUE))
+ })
 
 test_that("sim.data - factors are orthogonal", {
   set.seed(1)
