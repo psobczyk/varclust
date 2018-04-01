44b945726d0f477e10cdbcd29ad18db1dc26df28
sjwilczynski <sjwilczynski@gmail.com>
2018-04-01 21:13:51 +0200

    build fix?

--- tests/testthat/test_mlcc_reps.R
+++ tests/testthat/test_mlcc_reps.R
@@ -7,7 +7,6 @@ test_that("mlcc.reps on small matrix", {
   result <- varclust::mlcc.reps(X, max.dim = 2, numb.cores = 1)
   true_segmentation <- rep(1:2, each=50)
   scores <- varclust::integration(result$segmentation, true_segmentation)
-  print(scores)
   expect_gte(scores[1],0.9)
   expect_gte(scores[2],0.9)
 })
